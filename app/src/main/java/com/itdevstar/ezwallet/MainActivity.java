70c45fea560024fff0fd97c63b9078cb9dcc6144
ITDevStar <itdevstar@hotmail.com>
2019-07-09 00:51:24 +0300

    alert for save

--- app/src/main/java/com/itdevstar/ezwallet/MainActivity.java
+++ app/src/main/java/com/itdevstar/ezwallet/MainActivity.java
@@ -1,9 +1,11 @@
 package com.itdevstar.ezwallet;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,8 +21,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView m_wvMain;
-    JavaScriptInterface JSInterface;
+    private final String                        SAVE_PASSWORD_KEY = "save_password";
+    private final String                        USERNAME_KEY = "username";
+    private final String                        PASSWORD_KEY = "password";
+
+    private final int                           SAVE_PASSWORD_STATE_NOTNOW = 0;
+    private final int                           SAVE_PASSWORD_STATE_NEVER = 1;
+    private final int                           SAVE_PASSWORD_STATE_SAVED = 2;
+
+    private JavaScriptInterface                 JSInterface;
+
+    private WebView                             m_wvMain;
+    private String                              m_strCurUserName;
+    private String                              m_strCurPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,79 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
         m_wvMain = (WebView) findViewById(R.id.wv_main);
 
-      /*  m_wvMain = (WebView) findViewById(R.id.wv_main);
-        m_wvMain.getSettings().setJavaScriptEnabled(true);
-        m_wvMain.getSettings().setAppCacheEnabled(true);
-        m_wvMain.getSettings().setDomStorageEnabled(true);
-
-        m_wvMain.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // chromium, enable hardware acceleration
-            m_wvMain.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        } else {
-            // older android version, disable hardware acceleration
-            m_wvMain.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-
-
-        String myURL = "https://111.datatrium.com";
-
-        CookieSyncManager.createInstance(this);
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
-
-        WebView webview = new WebView(this);
-        WebSettings ws = webview.getSettings();
-        ws.setSaveFormData(true);
-        ws.setSavePassword(true); // Not needed for API level 18 or greater (deprecated)
-
-
-        m_wvMain.loadUrl(myURL);
-        m_wvMain.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-               // view.loadUrl(url);
-               // return true;
-                return false  ;
-            }
-        });*/
-
-       /* m_wvMain.requestFocus();
-
-        m_wvMain.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // chromium, enable hardware acceleration
-            m_wvMain.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        } else {
-            // older android version, disable hardware acceleration
-            m_wvMain.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-
-        CookieSyncManager.createInstance(this);
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
-
-        m_wvMain.getSettings().setJavaScriptEnabled(true);
-        m_wvMain.getSettings().setDomStorageEnabled(true);
-        m_wvMain.getSettings().setSavePassword(true);
-        m_wvMain.getSettings().setJavaScriptEnabled(true);
-        m_wvMain.getSettings().setAppCacheEnabled(true);
-        m_wvMain.getSettings().setSaveFormData(true);
-
-        m_wvMain.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-
-        //m_wvMain.loadUrl("https://111.datatrium.com/fmi/webd");
-
-        m_wvMain.loadUrl("https://111.datatrium.com");*/
-
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading Data...");
-        progressDialog.setCancelable(false);
+//        final ProgressDialog progressDialog = new ProgressDialog(this);
+//        progressDialog.setMessage("Loading Data...");
+//        progressDialog.setCancelable(false);
         m_wvMain = findViewById(R.id.wv_main);
         m_wvMain.requestFocus();
         m_wvMain.getSettings().setJavaScriptEnabled(true);
@@ -119,17 +62,63 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        m_wvMain.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                if (progress < 100) {
-                    progressDialog.show();
-                }
-                if (progress == 100) {
-                    progressDialog.dismiss();
-                }
-            }
-        });
+//        m_wvMain.setWebChromeClient(new WebChromeClient() {
+//            public void onProgressChanged(WebView view, int progress) {
+//                if (progress < 100) {
+//                    progressDialog.show();
+//                }
+//                if (progress == 100) {
+//                    progressDialog.dismiss();
+//                }
+//            }
+//        });
+
     }
+
+    private void checkState() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int state = sharedPreferences.getInt(SAVE_PASSWORD_KEY, SAVE_PASSWORD_STATE_NOTNOW);
+
+        if (state == SAVE_PASSWORD_STATE_NOTNOW) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Would you like to save this password for the app?").setPositiveButton("Save", dialogClickListener)
+                    .setNegativeButton("Never", dialogClickListener).setNeutralButton("Not Now", dialogClickListener).show();
+        }
+
+    }
+
+    private void saveState(int value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(SAVE_PASSWORD_KEY, value);
+        editor.commit();
+    }
+
+    private void saveUserInfo() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(USERNAME_KEY, m_strCurUserName);
+        editor.putString(PASSWORD_KEY, m_strCurPassword);
+        editor.commit();
+    }
+
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    saveState(SAVE_PASSWORD_STATE_SAVED);
+                    saveUserInfo();
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    saveState(SAVE_PASSWORD_STATE_NEVER);
+                    break;
+                case DialogInterface.BUTTON_NEUTRAL:
+                    saveState(SAVE_PASSWORD_STATE_NOTNOW);
+                    break;
+            }
+        }
+    };
 
     public class JavaScriptInterface {
         Context mContext;
@@ -145,15 +134,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @android.webkit.JavascriptInterface
-        public void SavePreferences(String key, String value){
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString(key, value);
-            editor.commit();
+        public void savePreferences(String username, String password){
+            m_strCurUserName = username;
+            m_strCurPassword = password;
+            checkState();
         }
 
         @android.webkit.JavascriptInterface
-        public String load_prefs(String key){
+        public String loadPreferences(String key){
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
             return sharedPreferences.getString(key, "");
         }
