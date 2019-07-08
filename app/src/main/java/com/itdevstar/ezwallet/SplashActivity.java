50a9f6bf98db641f3e582b965bc18eeda8de2023
ITDevStar <itdevstar@hotmail.com>
2019-07-08 07:49:26 +0300

    icon, splash

---
+++ app/src/main/java/com/itdevstar/ezwallet/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.itdevstar.ezwallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_SCREEN_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        scheduleSplashScreen();
+    }
+
+    private void scheduleSplashScreen() {
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_DELAY);
+    }
+}
